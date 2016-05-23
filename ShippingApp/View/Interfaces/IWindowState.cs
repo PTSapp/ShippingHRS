35f5e888c1076e0f4178ea674b811a1ab28c9439
Georgi Iliev <neokaozzz@gmail.com>
2016-05-23 15:34:25 +0300

    Updated skeleton comments.

--- ShippingApp/View/Interfaces/IWindowState.cs
+++ ShippingApp/View/Interfaces/IWindowState.cs
@@ -2,6 +2,9 @@
 {
     interface IWindowState
     {
+        /***
+        In case you`d like to manage the VISIBILITY of elements RATHER than creating multiple pages(one for admin view, one for employee view)
+        */
         bool VisibleForAll { get; }
     }
 }
